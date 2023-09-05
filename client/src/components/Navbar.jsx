8b43598fb9b4ef8042d96b65d1433f84013578d2
Bharath-KumarReddy <127650446+Bharath-KumarReddy@users.noreply.github.com>
2023-09-05 22:15:07 +0530

    Update Navbar.jsx

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,41 +1,76 @@
 import React from 'react';
-import {Link} from 'react-router-dom'
-import './Navbar1.css';
+import { Link } from 'react-router-dom';
+import './Navbar.css'; 
+
 const Navbar = () => {
   return (
     <div>
-      <nav className="navbar navbar-expand-lg bg-secondary">
-        <div className="container"> 
-          <Link className="navbar-brand" to="/" style={{color:"white"}}>Books Store</Link>
-          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div className="container">
+          <Link className="navbar-brand" to="/" style={{ color: "white" }}>
+            Books Store
+          </Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
             <span className="navbar-toggler-icon"></span>
           </button>
 
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav ml-auto">
-              <Link className="nav-item nav-link hovering" style={{color:"white"}} to="/home">
+              <Link
+                className="nav-item nav-link hovering"
+                style={{ color: "white" }}
+                to="/home"
+              >
                 HOME
               </Link>
-              <Link className="nav-item active nav-link hovering" style={{color:"white"}} to="/getall">
+              <Link
+                className="nav-item active nav-link hovering"
+                style={{ color: "white" }}
+                to="/getall"
+              >
                 BOOKS
               </Link>
-              <Link className="nav-item active nav-link hovering" style={{color:"white"}} to="/addBook">
+              <Link
+                className="nav-item active nav-link hovering"
+                style={{ color: "white" }}
+                to="/addBook"
+              >
                 Add Books
               </Link>
-              <Link className="nav-item active nav-link hovering" style={{color:"white"}} to="/getallusers">
+              <Link
+                className="nav-item active nav-link hovering"
+                style={{ color: "white" }}
+                to="/getallusers"
+              >
                 GetAll Users
-              </Link> 
-              <Link className="nav-item active nav-link hovering" style={{color:"white"}} to="/getrequest">
-                 Requests
               </Link>
-              <Link className="nav-item active nav-link hovering" style={{color:"white"}} to="/">
-                 Logout
+              <Link
+                className="nav-item active nav-link hovering"
+                style={{ color: "white" }}
+                to="/getrequest"
+              >
+                Requests
+              </Link>
+              <Link
+                className="nav-item active nav-link hovering"
+                style={{ color: "white" }}
+                to="/"
+              >
+                Logout
               </Link>
             </ul>
           </div>
         </div>
       </nav>
-      <div style={{ borderBottom: '2px solid white' }}></div>
+      <div className="nav-underline"></div>
     </div>
   );
 };
